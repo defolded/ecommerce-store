2d95054489488920cea9db498ffa3738f9e70587
Nikita <hailrake04@gmail.com>
2023-11-24 11:28:02 -0500

    added category page

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,76 @@
+import getCategory from "@/actions/get-category"
+import getColors from "@/actions/get-colors"
+import getProducts from "@/actions/get-products"
+import getSizes from "@/actions/get-sizes"
+import Billboard from "@/components/billboard"
+import Container from "@/components/ui/container"
+import NoResults from "@/components/ui/no-results"
+import ProductCard from "@/components/ui/product-card"
+import Filter from "./components/filter"
+import MobileFilters from "./components/mobile-filter"
+
+
+interface CategoryPageProps {
+    params: {
+        categoryId: string
+    }
+    searchParams: {
+        colorId: string
+        sizeId: string
+    }
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+    params,
+    searchParams
+}) => {
+    const prodcuts = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    })
+
+    const sizes = await getSizes()
+    const colors = await getColors()
+    const category = await getCategory(params.categoryId)
+
+  return (
+    <div className="bg-white">
+        <Container>
+            <Billboard
+                data={category.billboard}
+            />
+            <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                    <MobileFilters sizes={sizes} colors={colors} />
+                    <div className="hidden lg:block">
+                        <Filter
+                            valueKey='sizeId'
+                            name='Sizes'
+                            data={sizes}
+                        />
+                        <Filter
+                            valueKey='colorId'
+                            name='Colors'
+                            data={colors}
+                        />
+                    BM</div>
+                    <div className="mt-6 lg:col-span-4 lg:mt-0">
+                        {prodcuts.length === 0 && <NoResults />}
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                            {prodcuts.map((i) => (
+                                <ProductCard 
+                                    key={i.id}
+                                    data={i}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default CategoryPage
